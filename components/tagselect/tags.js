40ecb8191d4e6e0cd20233d0227c088ca488f8e7
Mengjun(Ivan) Wang <wmj900823@gmail.com>
2020-07-18 16:31:38 -0700

    refactor event binding and added util method for find a parent node with specified class

--- components/tagselect/tags.js
+++ components/tagselect/tags.js
@@ -1,17 +1,64 @@
+import { bindEvent, findParentWithClass } from '../../utils.js';
+
 function toggleDropDown(e) {
-  const outerContainer = e.target.parentNode.parentNode;
-  const dropdown = outerContainer.querySelector('.dropdown-list');
+  let tagselectContainer = findParentWithClass(e.target, 'tags-select');
+
+  if (!tagselectContainer) {
+    return;
+  }
+
+  const dropdown = tagselectContainer.querySelector('.dropdown-list');
   dropdown.classList.toggle('hidden');
 }
 
 function clearInput(e) {
-  const outerContainer = e.target.parentNode;
-  const inputs = outerContainer.querySelector('.inputs');
+  const tagselectContainer = findParentWithClass(e.target, 'tags-select');
+  if (!tagselectContainer) {
+    return;
+  }
+  const inputs = tagselectContainer.querySelector('.inputs');
   while (inputs.firstChild) {
     inputs.removeChild(inputs.lastChild);
   }
 }
 
 function closeTag(e) {
-  e.target.parentNode.remove();
+  const tagContainer = findParentWithClass(e.target, 'tag');
+  if (!tagContainer) {
+    return;
+  }
+  tagContainer.remove();
+}
+
+export default function bindTagSelectEvents() {
+  const dropdownControls = document.querySelectorAll('.dropdown-control');
+  bindEvent(dropdownControls, 'click', toggleDropDown);
+  //   for (let i = 0; i < dropdownControls.length; i++) {
+  //     dropdownControls[i].addEventListener('click', toggleDropDown);
+  //   }
+
+  const inputs = document.querySelectorAll('.inputs');
+  bindEvent(inputs, 'click', toggleDropDown);
+  //   for (let i = 0; i < inputs.length; i++) {
+  //     inputs[i].addEventListener('click', toggleDropDown);
+  //   }
+
+  const clearInputButtons = document.querySelectorAll('.clear-input');
+  bindEvent(clearInputButtons, 'click', clearInput);
+  //   for (let i = 0; i < clearInputButtons.length; i++) {
+  //     clearInputButtons[i].addEventListener('click', clearInput);
+  //   }
+
+  const tags = document.querySelectorAll('.tag');
+  bindEvent(tags, 'click', (e) => {
+    e.stopPropagation();
+  });
+  //   for (let i = 0; i < clearInputButtons.length; i++) {
+  //     tags[i].addEventListener('click', function (e) {
+  //       e.stopPropagation();
+  //     });
+  //   }
+
+  const closeTags = document.querySelectorAll('.close-tag');
+  bindEvent(closeTags, 'click', closeTag);
 }
